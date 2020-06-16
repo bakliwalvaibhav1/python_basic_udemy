968fa31fa725372b3014a2d221b834eea0929e39
shubhamdubile <dubileshubham@gmail.com>
2020-06-17 00:15:41 +0530

    added

---
+++ 3_Greatest_of_Three/greatest_of_three.py
@@ -0,0 +1,9 @@
+a= int(input('Enter First Number'))
+b= int(input('Enter Second Number'))
+c= int(input('Enter Third Number'))
+if b<= a >=c:
+    print(a)
+elif a<= b >=c:
+    print (b)
+else:
+    print(c)
